35bba5f86a2c49e151465676798749f3373fc7a7
Dhruv Diddi <dhruv.diddi+1@gmail.com>
2019-05-20 20:25:32 -0700

    Add virtual env directory, Import NLTK

---
+++ sentiment_analysis_simple.py
@@ -0,0 +1,5 @@
+# NLTK is a natural language processing library
+# Contains many useful corpus and algorithsm for language analysis
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import movie_reviews
